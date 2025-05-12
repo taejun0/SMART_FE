141b135a868e5414d7600d355ba0562d9ba32e5e
taejun0 <ohtaetaejun@gmail.com>
2025-05-12 16:57:54 +0900

    [Feat] #15 - 분석 페이지 ui 및 로직

---
+++ src/constants/anaylzeConstants.js
@@ -0,0 +1,44 @@
+import { IMAGE_CONSTANTS } from './imageConstants';
+
+export const ANALYZECONSTANTS = {
+  Header: {
+    TITLE: '훈련분석',
+  },
+  Section: {
+    HISTORY_TITLE: '최근 기록들이에요',
+  },
+  Feedback: {
+    PUSHUP: {
+      INCREASE: '팔굽혀펴기 횟수가 증가했어요!',
+      DECREASE: '팔굽혀펴기 횟수가 감소했어요!',
+    },
+    SITUP: {
+      INCREASE: '윗몸일으키기 횟수가 증가했어요!',
+      DECREASE: '윗몸일으키기 횟수가 감소했어요!',
+    },
+    RUNNING: {
+      INCREASE: '뜀걸음 시간이 감소했어요!',
+      DECREASE: '뜀걸음 시간이 증가했어요',
+    },
+    SHOOTING: {
+      INCREASE: '사격 횟수가 증가했어요!',
+      DECREASE: '사격 횟수가 감소했어요',
+    },
+  },
+  HistoryLabel: {
+    PUSHUP: '팔굽혀펴기',
+    SITUP: '윗몸일으키기',
+    RUNNING: '뜀걸음',
+    SHOOTING: '사격 점수',
+    UNIT: {
+      REP: '회',
+      TIME: '분',
+      SCORE: '점',
+    },
+  },
+  NG_DECREASE: '사격 횟수가 감소했어요',
+  GoTOTRAIN: '훈련하러 가기',
+  Image: {
+    Arrow: IMAGE_CONSTANTS.ARROW,
+  },
+};
--- src/pages/analyzepage/styled.js
+++ src/pages/analyzepage/styled.js
@@ -5,3 +5,60 @@ export const Wrapper = styled.div`
   min-height: 100vh;
   width: 100%;
 `;
+
+export const Contatiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+`;
+
+export const Header = styled.div`
+  position: relative;
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderText = styled.div`
+  ${({ theme }) => theme.fonts.Subtitle_Bold_16};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const Icon = styled.img`
+  position: absolute;
+  width: 30px;
+  left: 15px;
+  rotate: 180deg;
+  cursor: pointer;
+`;
+
+export const Title = styled.div`
+  ${({ theme }) => theme.fonts.Title_ExtraBold_20};
+  color: ${({ theme }) => theme.colors.green01};
+`;
+
+export const Count = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem 0;
+  gap: 3rem;
+`;
+
+export const Rap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TrainBTN = styled.div`
+  ${({ theme }) => theme.fonts.Body_Bold_14};
+  color: ${({ theme }) => theme.colors.beige01};
+  background-color: ${({ theme }) => theme.colors.green01};
+  border-radius: 12px;
+  padding: 14px 30px;
+  cursor: pointer;
+`;
