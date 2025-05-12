8f122854ee48573758cdd6235959d095cc2a7e75
taejun0 <ohtaetaejun@gmail.com>
2025-05-12 18:45:11 +0900

    [Test] - 모바일 뷰 테스트

--- src/pages/reportpage/ReportPage.jsx
+++ src/pages/reportpage/ReportPage.jsx
@@ -3,7 +3,12 @@ import * as S from './styled';
 import { Footer } from '@components/footer/Footer';
 
 export const ReportPage = () => {
-  return <S.Wrapper>리 포 트 페 이 지</S.Wrapper>;
+  return (
+    <S.Wrapper>
+      리 포 트 페 이 지
+      <Footer />
+    </S.Wrapper>
+  );
 };
 
 export default ReportPage;
--- src/pages/reportpage/styled.js
+++ src/pages/reportpage/styled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   position: relative;
-  height: calc(var(--vh, 1vh) * 100);
+  height: calc(var(--vh, 1vh) * 100 - 78px);
   width: 100%;
 `;
