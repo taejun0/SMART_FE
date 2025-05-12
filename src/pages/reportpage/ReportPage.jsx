f0290deb502aef7c7d47fa95fd0681c4d2b7c3fe
taejun0 <ohtaetaejun@gmail.com>
2025-05-12 18:43:52 +0900

    [Test] - 모바일 뷰 대응 테스트

--- src/pages/reportpage/ReportPage.jsx
+++ src/pages/reportpage/ReportPage.jsx
@@ -3,12 +3,7 @@ import * as S from './styled';
 import { Footer } from '@components/footer/Footer';
 
 export const ReportPage = () => {
-  return (
-    <S.Wrapper>
-      리 포 트 페 이 지
-      <Footer />
-    </S.Wrapper>
-  );
+  return <S.Wrapper>리 포 트 페 이 지</S.Wrapper>;
 };
 
 export default ReportPage;
