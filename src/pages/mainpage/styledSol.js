db987bd0af58e96380b70c5ba1318f4f6f311287
taejun0 <ohtaetaejun@gmail.com>
2025-05-12 18:02:53 +0900

    [Test] 모바일 웹 뷰 대응

--- src/pages/mainpage/styledSol.js
+++ src/pages/mainpage/styledSol.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   position: relative;
-  min-height: calc(var(--vh, 1vh) * 100);
+  min-height: calc(var(--vh, 1vh) * 100 - 4rem);
   width: 100%;
 `;
 
